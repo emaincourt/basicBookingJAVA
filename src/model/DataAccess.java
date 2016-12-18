4ee928398c93bc2bc4f4090b12edd0c0f0388a34
Elliot Maincourt <e.maincourt@gmail.com>
2016-12-18 23:31:37 +0100

    Fixed minor bugs

--- src/model/DataAccess.java
+++ src/model/DataAccess.java
@@ -32,7 +32,6 @@ public class DataAccess {
   public static void main(String[] args) throws DataAccessException, ClassNotFoundException, SQLException{
     
     DataAccess myDataAccess = new DataAccess("jdbc:mysql://localhost:8889/booking","root","root");
-    BookingInfo bi = myDataAccess.book("Elliot", 1, 0, true);
     BookingInfo bi2 = myDataAccess.book("Yi-Han", 2, 2, true);
     BookingInfo bi3 = myDataAccess.book("Samuel", 0, 3, true);
     //BookingInfo bi4 = myDataAccess.cancel("Samuel", 1, 1);
@@ -96,8 +95,8 @@ public class DataAccess {
   
   public void createTriggerBeforeBooking() throws SQLException{
       try{
-        String addCustomer = "CREATE TRIGGER `before_booking_update` BEFORE UPDATE ON `BOOKINGS` FOR EACH ROW BEGIN IF (SELECT COUNT(*) FROM `ORDERS` WHERE CUSTOMER = NEW.CUSTOMER) = 0 THEN INSERT INTO `ORDERS` VALUES (NEW.CUSTOMER,0,NOW()); END IF; END";
-        String updateCustomerAmount = "CREATE TRIGGER `after_booking_update` AFTER UPDATE ON `BOOKINGS` FOR EACH ROW BEGIN  UPDATE ORDERS SET AMOUNT = (SELECT PRICE FROM PRICES WHERE CLASS = 1)*(SELECT COUNT(*) FROM BOOKINGS WHERE CLASS = 1 AND CUSTOMER = NEW.CUSTOMER)+(SELECT PRICE FROM PRICES WHERE CLASS = 2)*(SELECT COUNT(*) FROM BOOKINGS WHERE CLASS = 2 AND CUSTOMER = NEW.CUSTOMER) WHERE CUSTOMER = NEW.CUSTOMER; END;";
+        String addCustomer = "CREATE TRIGGER `before_booking_update` BEFORE UPDATE ON `BOOKINGS` FOR EACH ROW BEGIN IF (SELECT COUNT(*) FROM `ORDERS` WHERE CUSTOMER = NEW.CUSTOMER) = 0 AND NEW.CUSTOMER IS NOT NULL THEN INSERT INTO `ORDERS` VALUES (NEW.CUSTOMER,0,NOW()); END IF; END";
+        String updateCustomerAmount = "CREATE TRIGGER `after_booking_update` AFTER UPDATE ON `BOOKINGS` FOR EACH ROW BEGIN IF NEW.CUSTOMER IS NOT NULL THEN UPDATE ORDERS SET AMOUNT = (SELECT PRICE FROM PRICES WHERE CLASS = 1)*(SELECT COUNT(*) FROM BOOKINGS WHERE CLASS = 1 AND CUSTOMER = NEW.CUSTOMER)+(SELECT PRICE FROM PRICES WHERE CLASS = 2)*(SELECT COUNT(*) FROM BOOKINGS WHERE CLASS = 2 AND CUSTOMER = NEW.CUSTOMER) WHERE CUSTOMER = NEW.CUSTOMER; END IF; END;";
         ps = this.conn.prepareStatement(addCustomer);
         ps.execute();
         if(ps != null)
